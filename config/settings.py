37fe82ccee65fd75fbdfd48ce49609b5109f36cb
Taha Yıldırım <taha0yldrm@outlook.com>
2026-09-06 19:30:02 +0300

    added new app to Installed Apps

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'accounts.Account'
 
 # Application definition
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.accounts',
 ]
 
 MIDDLEWARE = [
