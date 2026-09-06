8e1e3dd79ced711b165d1abed068b018effc00cf
Taha Yıldırım <taha0yldrm@outlook.com>
2026-09-06 19:37:10 +0300

    fix include url

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('apps.accounts.urls')),
 ]
