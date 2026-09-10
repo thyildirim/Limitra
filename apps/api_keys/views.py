75151c9c3d8c507c228d0b16c7d5b7591e64abe0
Taha Yıldırım <taha0yldrm@outlook.com>
2026-09-10 21:07:57 +0300

    create views

--- apps/api_keys/views.py
+++ apps/api_keys/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from apps.api_keys.models import ApiKey
+from apps.api_keys.serializers import ApiKeySerializer
+from apps.projects.models import Project
+
+
+class ApiKeyListCreateView(generics.ListCreateAPIView): # list and create API view for API keys
+    serializer_class = ApiKeySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return ApiKey.objects.filter(
+            project_id = self.kwargs["project_id"],
+            project__owner=self.request.user
+        )
+
+    def perform_create(self, serializer):
+        project = get_object_or_404(
+            Project,
+            id=self.kwargs["project_id"],
+            owner=self.request.user
+        )
+
+        serializer.save(
+            project=project,
+            key = ApiKey.generate_key()
+        )
+
+
+class ApiKeyDetailView(generics.RetrieveUpdateDestroyAPIView): # retrieve, update, and delete API view for API keys
+    serializer_class = ApiKeySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return ApiKey.objects.filter(
+            project__owner=self.request.user
+        )
